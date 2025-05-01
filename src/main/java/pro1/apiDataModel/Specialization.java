4f8c39979db0973caee2f6b2d526821142dd0c21
Patrik Vuong <Patrikvuongs@seznam.cz>
2025-05-01 19:24:01 +0200

    snad funkcni

--- src/main/java/pro1/apiDataModel/Specialization.java
+++ src/main/java/pro1/apiDataModel/Specialization.java
@@ -1,7 +1,14 @@
 package pro1.apiDataModel;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Specialization {
+    @SerializedName("programNazev")
     public String programNazev;
+
+    @SerializedName("oborNazev")
     public String oborNazev;
+
+    @SerializedName("eprDeadlinePrihlaska")
     public Deadline eprDeadlinePrihlaska;
 }
